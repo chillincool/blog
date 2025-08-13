c28c2c7e8c6fbcc3ed9642cc7cd5e3d35cadbada
chillincool <chris@chillincool.net>
2025-08-12 21:26:28 -0400

    fix again...

--- components/mdx/Lucidchart.tsx
+++ components/mdx/Lucidchart.tsx
@@ -9,7 +9,7 @@ export default function Lucidchart({
 }) {
   return (
     <div className="my-6">
-      <iframe title={title} src={src} width={width} height={height} style={{ border: 0 }} allowFullScreen />
+      <iframe title="Lucidchart Diagram" src={src} width={width} height={height} style={{ border: 0 }} allowFullScreen />
     </div>
   )
 }
